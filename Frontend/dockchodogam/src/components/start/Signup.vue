a182c9e05eb8df43137543dececa9fedbce5a707
seosanggyun <seosanggyun@naver.com>
2022-09-21 17:03:29 +0900

    ADD signup

--- Frontend/dockchodogam/src/components/start/Signup.vue
+++ Frontend/dockchodogam/src/components/start/Signup.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-    <input v-model="nickname" placeholder="닉네임" />
+    <div>
+      <input v-model="nickname" placeholder="닉네임" />
+      <button type="submit" @click="isNicknameDuplicate()">
+        닉네임중복확인
+      </button>
+    </div>
     <input v-model="username" placeholder="아이디" />
-    <input v-model="email" placeholder="이메일" />
+    <div>
+      <input v-model="email" placeholder="이메일" />
+      <button type="submit" @click="isEmailDuplicate()">이메일중복확인</button>
+    </div>
     <input v-model="region" placeholder="리젼" />
-    <input v-model="password" type="password" placeholder="비밀번호" />
-    <input v-model="password2" type="password" placeholder="비밀번호 확인" />
+    <div>
+      <input v-model="password" type="password" placeholder="비밀번호" />
+      <input v-model="password2" type="password" placeholder="비밀번호 확인" />
+    </div>
     <button type="submit" @click="signup()">회원가입</button>
   </div>
 </template>
@@ -20,23 +30,63 @@ export default {
       password: this.password,
       password2: this.password2,
       username: this.username,
-      region: this.region
+      region: this.region,
+      nicknameDuplicate: true,
+      emailDuplicate: true
     }
   },
   methods: {
+    async isEmailDuplicate() {
+      await axios
+        .get(
+          'http://localhost:8081/api/v1/user/auth/check/email/' + this.email,
+          {
+            email: this.email
+          }
+        )
+        .then((res) => {
+          console.log(res)
+          if (res.data === false) {
+            this.emailDuplicate = false
+            alert('이 이메일은 사용하셔도 좋아용.')
+          } else {
+            this.emailDuplicate = true
+            alert('이미 존재하는 이메일 주소입니다.')
+          }
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    },
+    async isNicknameDuplicate() {
+      await axios
+        .get(
+          'http://localhost:8081/api/v1/user/auth/check/nickname/' +
+            this.nickname,
+          {
+            nickname: this.nickname
+          }
+        )
+        .then((res) => {
+          console.log(res)
+          if (res.data === false) {
+            this.nicknameDuplicate = false
+            alert('이 닉네임은 사용하셔도 좋아용.')
+          } else {
+            this.nicknameDuplicate = true
+            alert('이미 존재하는 닉네임입니다.')
+          }
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    },
     async signup() {
-      if (this.password === this.password2) {
-        // await axios({
-        //   url: 'http://localhost:8081/api/v1/user/auth/signup',
-        //   method: 'post',
-        //   body: {
-        //     email: this.email,
-        //     nickname: this.nickname,
-        //     password: this.password,
-        //     region: this.region,
-        //     username: this.username
-        //   }
-        // })
+      if (this.nicknameDuplicate === true) {
+        alert('닉네임중복검사를 먼저 진행해주세요.')
+      } else if (this.emailDuplicate === true) {
+        alert('이메일중복검사를 먼저 진행해주세요.')
+      } else if (this.password === this.password2) {
         await axios
           .post('http://localhost:8081/api/v1/user/auth/signup', {
             email: this.email,
@@ -52,7 +102,7 @@ export default {
             console.log(err)
           })
       } else {
-        return alert('비밀번호가 일치하지 않습니다')
+        return alert('비밀번호가 일치하지 않습니다.')
       }
     }
   }
