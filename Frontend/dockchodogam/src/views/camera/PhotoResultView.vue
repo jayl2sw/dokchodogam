a606292bbde8383dd1a7abad914d8ec7728d1d08
박지현 <ms_google@naver.com>
2022-09-22 10:39:20 +0900

    ADD dockcho warning photo result add warning #park

--- Frontend/dockchodogam/src/views/camera/PhotoResultView.vue
+++ Frontend/dockchodogam/src/views/camera/PhotoResultView.vue
@@ -24,6 +24,9 @@
         <div class="dockchoExplanation__container">
           <div class="dockchoExplanation">
             <img src="@/assets/flower_ex.png" alt="flower" />
+            <h3 v-if="results.docko == true">
+              독초입니다! 채집 및 섭취에 주의하세요.
+            </h3>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod nobis
             ea quae omnis doloribus assumenda impedit, maiores id tempora
             corporis obcaecati quis minima magnam sapiente ipsa in dolores hic
