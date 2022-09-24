6758273df37ef0dd8fa3a50678aaaba1756fe012
김성빈 <ksb5535@naver.com>
2022-09-24 16:03:33 +0900

    FIX Oauth2 로그인 설정 수정

--- Backend/DockChoDoGam/src/main/java/com/ssafy/dockchodogam/service/oauth2/OAuth2SuccessHandler.java
+++ Backend/DockChoDoGam/src/main/java/com/ssafy/dockchodogam/service/oauth2/OAuth2SuccessHandler.java
@@ -1,7 +1,14 @@
 package com.ssafy.dockchodogam.service.oauth2;
 
+import com.ssafy.dockchodogam.domain.Deck;
+import com.ssafy.dockchodogam.domain.Monster;
+import com.ssafy.dockchodogam.domain.UserMonster;
 import com.ssafy.dockchodogam.domain.enums.Role;
 import com.ssafy.dockchodogam.domain.User;
+import com.ssafy.dockchodogam.dto.exception.game.MonsterNotFoundException;
+import com.ssafy.dockchodogam.repository.DeckRepository;
+import com.ssafy.dockchodogam.repository.MonsterRepository;
+import com.ssafy.dockchodogam.repository.UserMonsterRepository;
 import com.ssafy.dockchodogam.repository.UserRepository;
 import com.ssafy.dockchodogam.dto.jwt.TokenDto;
 import com.ssafy.dockchodogam.jwt.TokenProvider;
@@ -24,6 +31,9 @@ import java.util.Map;
 public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
     private final TokenProvider tokenProvider;
     private final UserRepository userRepository;
+    private final MonsterRepository monsterRepository;
+    private final UserMonsterRepository userMonsterRepository;
+    private final DeckRepository deckRepository;
     private final PasswordMaker passwordMaker;
 
     @Override
@@ -40,7 +50,6 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
         // db 확인
         // 새로운 유저 -> 회원가입
-        // 변경해야함!!!!
         User entity = userRepository.findByEmail(email)
                 .orElse(User.builder()
                         .username(passwordMaker.make())
@@ -48,7 +57,21 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
                         .email(email)
                         .nickname(nickname)
                         .role(Role.ROLE_USER)
+                        .representMonster(monsterRepository.findById(Long.parseLong("8")).orElseThrow(MonsterNotFoundException::new))
                         .build());
+
+        Monster zero = monsterRepository.findById(Long.parseLong("0")).orElseThrow(MonsterNotFoundException::new);
+
+        Deck deck = Deck.builder()
+                .user(entity)
+                .monster1(zero).monster2(zero).monster3(zero).monster4(zero).monster5(zero).build();
+
+        deckRepository.save(deck);
+
+        // 기본 독초몬 지급하기
+        userMonsterRepository.save(UserMonster.builder().user(entity).monster(monsterRepository.findById(Long.parseLong("8")).orElseThrow(MonsterNotFoundException::new)).build());
+        userMonsterRepository.save(UserMonster.builder().user(entity).monster(monsterRepository.findById(Long.parseLong("46")).orElseThrow(MonsterNotFoundException::new)).build());
+        userMonsterRepository.save(UserMonster.builder().user(entity).monster(monsterRepository.findById(Long.parseLong("80")).orElseThrow(MonsterNotFoundException::new)).build());
 
         // 토큰 생성
         TokenDto tokenDto = tokenProvider.generateTokenDto(entity.getEmail(), entity.getRole().toString());
@@ -56,8 +79,7 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
         userRepository.save(entity);
 
         // 리다이렉트
-        // 주소 변경해야함!!!!
-        String target = "http://localhost:3000/oauth?Auth=" + tokenDto.getAccessToken() + "&Refresh=" + tokenDto.getRefreshToken();
+        String target = "http://j7e201.p.ssafy.io/oauth?Auth=" + tokenDto.getAccessToken() + "&Refresh=" + tokenDto.getRefreshToken();
         RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
         redirectStrategy.sendRedirect(request, response, target);
     }
