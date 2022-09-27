8c4e3a69ec553567777a217d54bb4e6b215c4dc8
박지현 <ms_google@naver.com>
2022-09-27 22:24:55 +0900

    FIX monstercard little fixed monstercard css #park

--- Frontend/dockchodogam/src/components/encyclopedia/MonsterCard.vue
+++ Frontend/dockchodogam/src/components/encyclopedia/MonsterCard.vue
@@ -1,6 +1,30 @@
 <template>
+  <div class="container">
+    <div class="card">
+      <div class="imgBx">
+        <img
+          :src="this.imageBaseUrl + '/' + monster.monsterId + '.png'"
+          class="card__img"
+          style="-webkit-user-drag: none"
+        />
+      </div>
+      <div class="contentBx">
+        <p>00{{ monster.monsterId }}</p>
+        <h2>{{ monster.name }}몬</h2>
+        <div class="size">
+          <p>
+            <br />
+            타입 : {{ this.monster.type }} <br />등급 :
+            {{ this.monster.grade }} <br />
+            체력 : {{ this.monster.hp }} <br />
+            공격력 : {{ this.monster.minAttack }} ~ {{ this.monster.maxAttack }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
   <!-- isGot 확인해서 v-if 걸기 -->
-  <div v-if="monster.got == true && monster.monsterId !== 0" class="card">
+  <!-- <div v-if="monster.got == true && monster.monsterId !== 0" class="card">
     <div
       v-b-modal.modal-card
       @click=";[storeMonster(monster), openDetail()]"
@@ -12,7 +36,7 @@
         card__special: monster.grade == 'SPECIAL'
       }"
     >
-      <p>00{{ monster.monsterId }}</p>
+      <h3>00{{ monster.monsterId }}</h3>
       <h3>{{ monster.name }}몬</h3>
       <img
         :src="this.imageBaseUrl + '/' + monster.monsterId + '.png'"
@@ -32,7 +56,7 @@
         style="-webkit-user-drag: none"
       />
     </div>
-  </div>
+  </div> -->
 </template>
 
 <script>
@@ -107,11 +131,187 @@ export default {
 </script>
 
 <style scoped>
-.card {
+body {
   display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background: #131313;
+}
+
+.container {
+  position: relative;
+}
+
+.container .card {
+  position: relative;
+  width: 320px;
+  height: 450px;
+  background: #232323;
+  border-radius: 20px;
+  overflow: hidden;
+}
+
+/* .container .card:before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: #9bdc28;
+  clip-path: circle(150px at 80% 20%);
+  transition: 0.5s ease-in-out;
+} */
+
+/* .container .card:hover:before {
+  clip-path: circle(300px at 80% -20%);
+} */
+
+/* .container .card:after {
+  content: 'Nike';
+  position: absolute;
+  top: 30%;
+  left: -20%;
+  font-size: 12em;
+  font-weight: 800;
+  font-style: italic;
+  color: rgba(255, 255, 25, 0.05);
+} */
+
+.container .card .imgBx {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  z-index: 10000;
+  width: 100%;
+  height: 220px;
+  transition: 0.5s;
+}
+
+.container .card:hover .imgBx {
+  top: 0%;
+  transform: translateY(0%);
+}
+
+.container .card .imgBx img {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%) rotate(-25deg);
+  width: 270px;
+}
+
+.container .card .contentBx {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 100px;
+  text-align: center;
+  transition: 1s;
+  z-index: 10;
+}
+
+.container .card:hover .contentBx {
+  height: 210px;
+}
+
+.container .card .contentBx h2 {
+  position: relative;
+  font-weight: 600;
+  letter-spacing: 1px;
+  color: #fff;
+  margin: 0;
+}
+
+.container .card .contentBx .size,
+.container .card .contentBx .color {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 8px 20px;
+  transition: 0.5s;
+  opacity: 0;
+  visibility: hidden;
+  padding-top: 0;
+  padding-bottom: 0;
+  color: white;
+}
+
+.container .card:hover .contentBx .size {
+  opacity: 1;
+  visibility: visible;
+  transition-delay: 0.5s;
+}
+
+.container .card:hover .contentBx .color {
+  opacity: 1;
+  visibility: visible;
+  transition-delay: 0.6s;
+}
+
+.container .card .contentBx .size h3,
+.container .card .contentBx .color h3 {
+  color: #fff;
+  font-weight: 300;
+  font-size: 14px;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  margin-right: 10px;
+}
+
+.container .card .contentBx .size span {
+  width: 26px;
+  height: 26px;
+  text-align: center;
+  line-height: 26px;
+  font-size: 14px;
+  display: inline-block;
+  color: #111;
+  background: #fff;
+  margin: 0 5px;
+  transition: 0.5s;
+  color: #111;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+/* .container .card .contentBx .size span:hover {
+  background: #9bdc28;
+} */
+
+.container .card .contentBx a {
+  display: inline-block;
+  padding: 10px 20px;
+  background: #fff;
+  border-radius: 4px;
+  margin-top: 10px;
+  text-decoration: none;
+  font-weight: 600;
+  color: rgb(255, 254, 254);
+  opacity: 0;
+  transform: translateY(50px);
+  transition: 0.5s;
+  margin-top: 0;
+}
+
+.container .card:hover .contentBx a {
+  opacity: 1;
+  transform: translateY(0px);
+  transition-delay: 0.75s;
+}
+.card {
+  position: relative;
   width: 20vw;
   height: 50vh;
+  margin: 0 auto;
+  background: #000;
+  border-radius: 15px;
+  box-shadow: 0 15px 60px rgba(0, 0, 0, 0.5);
+  display: flex;
 }
+
 .card__img {
   width: 100%;
   height: 20vh;
