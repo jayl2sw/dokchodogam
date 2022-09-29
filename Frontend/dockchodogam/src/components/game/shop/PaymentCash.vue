bbcfdc8cc6fe826db191db73c0466ac1d20a6174
박지현 <ms_google@naver.com>
2022-09-29 12:10:59 +0900

    ADD css shop animationview edit #park

--- Frontend/dockchodogam/src/components/game/shop/PaymentCash.vue
+++ Frontend/dockchodogam/src/components/game/shop/PaymentCash.vue
@@ -43,8 +43,8 @@ export default {
           // pay_method: 'card', // 결제수단
           merchant_uid: `mid_${new Date().getTime()}`, // 주문번호
           amount: 100, // 결제금액
-          name: '재화충전:결제테스트', // 주문명
-          m_redirect_url: 'https://j7e201.p.ssafy.io/',
+          name: '재화충전:1,000냥', // 주문명
+          // m_redirect_url: 'https://j7e201.p.ssafy.io/game/shop',
           buyer_name: `${this.userInfo.username}` // 구매자 이름 //이거 username으로 넣기
         },
         (rsp) => {
