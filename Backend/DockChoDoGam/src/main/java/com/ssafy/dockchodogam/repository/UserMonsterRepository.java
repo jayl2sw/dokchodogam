b53c142d9a6f998de1b6f18bb2f687db5f105385
이재준 <jayl2cu@gmail.com>
2022-09-29 17:39:37 +0900

    UPDATE image judge

--- Backend/DockChoDoGam/src/main/java/com/ssafy/dockchodogam/repository/UserMonsterRepository.java
+++ Backend/DockChoDoGam/src/main/java/com/ssafy/dockchodogam/repository/UserMonsterRepository.java
@@ -1,16 +1,22 @@
 package com.ssafy.dockchodogam.repository;
 
 import com.ssafy.dockchodogam.domain.Monster;
+import com.ssafy.dockchodogam.domain.User;
 import com.ssafy.dockchodogam.domain.UserMonster;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserMonsterRepository extends JpaRepository<UserMonster, Long> {
 
     Optional<UserMonster> findUserMonsterByMonsterMonsterIdAndUserUserId(Long monsterId, Long userId);
+    Optional<UserMonster> findUserMonsterByMonsterAndUser(Monster monster, User user);
+
     Page<UserMonster> findUserMonsterByUserUserId(Long userId, Pageable pageable);
 
 }
