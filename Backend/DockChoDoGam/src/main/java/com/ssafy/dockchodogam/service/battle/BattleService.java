8176a559869e35f453e73b9580fc8807d4087492
김성빈 <ksb5535@gmail.com>
2022-09-29 16:17:20 +0900

    ADD 전적 검색

--- Backend/DockChoDoGam/src/main/java/com/ssafy/dockchodogam/service/battle/BattleService.java
+++ Backend/DockChoDoGam/src/main/java/com/ssafy/dockchodogam/service/battle/BattleService.java
@@ -1,13 +1,20 @@
 package com.ssafy.dockchodogam.service.battle;
 
+import com.ssafy.dockchodogam.dto.battle.BattleDto;
 import com.ssafy.dockchodogam.dto.battle.BattleLogRequestDto;
 import com.ssafy.dockchodogam.dto.battle.BattleRequestDto;
 import com.ssafy.dockchodogam.dto.battle.BattleStatusDto;
+import com.ssafy.dockchodogam.dto.gg.GGRequestDto;
+import com.ssafy.dockchodogam.dto.gg.WinRateDto;
 import org.springframework.data.crossstore.ChangeSetPersister;
+
+import java.util.List;
 
 public interface BattleService {
 
     Long createNewBattle(BattleRequestDto data);
     void createNewBattleLog(BattleLogRequestDto data);
     void finishBattle(BattleStatusDto data) throws ChangeSetPersister.NotFoundException;
+    List<BattleDto> searchLog(String nickname, int page);
+    WinRateDto getWinRate(String nickname);
 }
--- Backend/DockChoDoGam/src/main/java/com/ssafy/dockchodogam/service/battle/BattleServiceImpl.java
+++ Backend/DockChoDoGam/src/main/java/com/ssafy/dockchodogam/service/battle/BattleServiceImpl.java
@@ -2,19 +2,27 @@ package com.ssafy.dockchodogam.service.battle;
 
 import com.ssafy.dockchodogam.domain.Battle;
 import com.ssafy.dockchodogam.domain.BattleLog;
+import com.ssafy.dockchodogam.domain.User;
 import com.ssafy.dockchodogam.dto.battle.BattleDto;
 import com.ssafy.dockchodogam.dto.battle.BattleLogRequestDto;
 import com.ssafy.dockchodogam.dto.battle.BattleRequestDto;
 import com.ssafy.dockchodogam.dto.battle.BattleStatusDto;
+import com.ssafy.dockchodogam.dto.exception.user.UserNotFoundException;
+import com.ssafy.dockchodogam.dto.gg.GGRequestDto;
+import com.ssafy.dockchodogam.dto.gg.WinRateDto;
 import com.ssafy.dockchodogam.repository.BattleLogRepository;
 import com.ssafy.dockchodogam.repository.BattleRepository;
 import com.ssafy.dockchodogam.repository.MonsterRepository;
 import com.ssafy.dockchodogam.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -44,8 +52,8 @@ public class BattleServiceImpl implements BattleService {
                 .monster8(monsterRepository.findMonsterByMonsterId(data.getMonster8()))
                 .monster9(monsterRepository.findMonsterByMonsterId(data.getMonster9()))
                 .isRank(data.isRank())
-                .success(false)
-                .wellFinished(false)
+                .success(data.isSuccess())
+                .wellFinished(data.isFinish())
                 .build();
 
         System.out.println(battle);
@@ -83,5 +91,31 @@ public class BattleServiceImpl implements BattleService {
         BattleDto battleDto = new BattleDto().from(battle);
 //        battleDtoKafkaTemplate.send("battles", null, battleDto);
 
+    }
+
+    @Override
+    public List<BattleDto> searchLog(String nickname, int page){
+        Long userId = userRepository.findByNickname(nickname).orElseThrow(UserNotFoundException::new).getUserId();
+        return battleRepository.findBattlesByUser(userId, PageRequest.of(page, 20)).stream().map(battle -> BattleDto.from(battle)).collect(Collectors.toList());
+    }
+
+    @Override
+    public WinRateDto getWinRate(String nickname){
+        Long userId = userRepository.findByNickname(nickname).orElseThrow(UserNotFoundException::new).getUserId();
+        int attackCnt = battleRepository.findAttackCountByUser(userId);
+        int attackSuccess = battleRepository.findAttackSuccessCountByUser(userId);
+        int defenceCnt = battleRepository.findDefenceCountByUser(userId);
+        int defenceSuccess = battleRepository.findDefenceSuccessCountByUser(userId);
+        int totalCnt = attackCnt + defenceCnt;
+        double winRate = (double) (attackSuccess + defenceSuccess) / (double) totalCnt;
+
+        return WinRateDto.builder()
+                .totalGames(totalCnt)
+                .attackCnt(attackCnt)
+                .winAttack(attackSuccess)
+                .defenceCnt(defenceCnt)
+                .winDefence(defenceSuccess)
+                .winRate(winRate)
+                .build();
     }
 }
