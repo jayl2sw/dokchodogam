086f2de317daf3c09bce899393c7f8b6152184a3
박지현 <ms_google@naver.com>
2022-09-30 16:00:21 +0900

    FIX shop userinfo userinfo into nowuserinfo #park

--- Frontend/dockchodogam/src/components/game/shop/PaymentPackage.vue
+++ Frontend/dockchodogam/src/components/game/shop/PaymentPackage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="userInfo.starter == true" class="package">
+  <div v-if="nowUserInfo.starter == true" class="package">
     <div class="package__header">
       <h3 class="TITLE">🐣 스타터팩 🐣</h3>
       <p>⭐계정 당 1회 한정</p>
@@ -37,15 +37,15 @@ IMP.init('imp40805235')
 export default {
   data() {
     return {
-      userInfo: JSON.parse(localStorage.getItem('userInfo')),
+      // userInfo: JSON.parse(localStorage.getItem('userInfo')),
       packageMonsters: {}
     }
   },
   computed: {
-    ...mapGetters(['userInfo'])
+    ...mapGetters(['nowUserInfo'])
   },
   methods: {
-    ...mapActions(['monsterPackage, fetchUserInfo']),
+    ...mapActions(['monsterPackage, fetchnowUserInfo']),
     onPaymentPackage: function () {
       /* 1. 가맹점 식별하기 */
       // const IMP = window.IMP
@@ -76,7 +76,7 @@ export default {
                 // 몬스터 3개 정보 올 것,, 아마도 ?
                 // 담아서 PackageAnimationView으로 넘기기
                 this.fetchMonsterPackage()
-                this.fetchUserInfo()
+                // this.fetchnowUserInfo()
                 // this.packageMonsters = data.data
                 this.$router.replace({
                   path: '/game/shop/package'
@@ -85,7 +85,7 @@ export default {
               })
               .catch((err) => console.log(err))
           } else {
-            this.fetchUserInfo()
+            this.fetchnowUserInfo()
             // 결제 실패시 로직
             swal({
               title: '결제에 실패하였습니다 😢',
