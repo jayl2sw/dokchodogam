2c9b2fe1f4be833122b5d8b2fa25d7919dfff134
이재준 <jayl2cu@gmail.com>
2022-10-04 14:59:59 +0900

    UPDATE kafka bootstrapserver

--- Backend/DockChoDoGam/src/main/java/com/ssafy/dockchodogam/kafka/BattleProducerConfig.java
+++ Backend/DockChoDoGam/src/main/java/com/ssafy/dockchodogam/kafka/BattleProducerConfig.java
@@ -13,7 +13,7 @@ import java.util.Map;
 @Configuration
 public class BattleProducerConfig {
 
-    @Value("localhost:9092")
+    @Value("localhost:29092")
     private String bootstrapServer;
 
     @Bean
