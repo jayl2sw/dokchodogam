81be8d6c6de91eddb531d196d441c39ee26b0d43
오하민 <ohm55545@naver.com>
2022-10-04 12:53:15 +0900

    Revert "ADD PWA"
    This reverts commit ae36f913817075d92d94d611fc6db73db7809586.

--- Frontend/dockchodogam/src/main.ts
+++ Frontend/dockchodogam/src/main.ts
@@ -8,7 +8,6 @@ import { fas } from '@fortawesome/free-solid-svg-icons'
 import BootstrapVue3 from 'bootstrap-vue-3'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
-import './registerServiceWorker'
 // import Camera from 'simple-vue-camera'
 
 library.add(fas)
