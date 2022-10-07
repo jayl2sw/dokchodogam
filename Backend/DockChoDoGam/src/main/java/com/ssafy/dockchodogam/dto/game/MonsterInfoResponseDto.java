b256ec535939eb169555b3231e36519d7b2e46d0
이재준 <jayl2cu@gmail.com>
2022-10-07 09:56:23 +0900

    UPDATE back

--- Backend/DockChoDoGam/src/main/java/com/ssafy/dockchodogam/dto/game/MonsterInfoResponseDto.java
+++ Backend/DockChoDoGam/src/main/java/com/ssafy/dockchodogam/dto/game/MonsterInfoResponseDto.java
@@ -7,7 +7,6 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
-import javax.persistence.Column;
 
 @Getter
 @Builder
--- Backend/DockChoDoGam/src/main/java/com/ssafy/dockchodogam/repository/MonsterRepository.java
+++ Backend/DockChoDoGam/src/main/java/com/ssafy/dockchodogam/repository/MonsterRepository.java
@@ -1,14 +1,11 @@
 package com.ssafy.dockchodogam.repository;
 
 import com.ssafy.dockchodogam.domain.Monster;
-import com.ssafy.dockchodogam.domain.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-import java.util.Optional;
 
 public interface MonsterRepository extends JpaRepository<Monster, Long> {
 
